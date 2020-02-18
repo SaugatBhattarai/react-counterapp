52de38183956b5f68dcb809c150c5775f45066d5
Saugat Bhattarai <the.saggitorres@gmail.com>
2020-02-18 12:24:39 +0545

    creating funtional component to navbar and object destructuring

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,30 @@
 import React, { Component } from "react";
-class NavBar extends Component {
-  render() {
-    // console.log(this.props.totalCounters);
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar{" "}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+
+// const NavBar = props => {
+//   return (
+//     <nav className="navbar navbar-light bg-light">
+//       <a className="navbar-brand" href="#">
+//         Navbar{" "}
+//         <span className="badge badge-pill badge-secondary">
+//           {props.totalCounters}
+//         </span>
+//       </a>
+//     </nav>
+//   );
+// };
+
+//Props with object destructuring
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
